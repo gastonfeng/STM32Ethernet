08bd972ad025d4cef7d49154b2bd452094eccd04
gastonfeng <gastonfeng@gmail.com>
2019-10-12 17:25:37 +0800

    u

--- src/http_download.cpp
+++ src/http_download.cpp
@@ -25,7 +25,7 @@ int download(const char *host, uint16_t port, const char *url, const char *filen
         // create buffer for read
         uint8_t cur_buffer[CHUNK_SIZE];
         bool success = true;
-        FlashFile fil(filename, SPIFFS_O_CREAT | SPIFFS_WRONLY);
+        FlashFile fil(filename, FA_READ);
         // read all data from server
         int last_tick = millis();
         while (success && http.connected() && (downloadRemaining > 0))
